9be78e63f7c621c6af9cb52703ef69204d860a77
claudiom4sir <claudio_rota@hotmail.it>
2018-08-31 17:04:52 +0200

    added player and background

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager singleton;
+
+    [SerializeField] GameObject wallPrefab;
+
+    void Awake()
+    {
+        if (singleton == null)
+            singleton = this;
+        else
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        DrawWall();
+    }
+
+    void DrawWall()
+    {
+
+    }
+
+}
